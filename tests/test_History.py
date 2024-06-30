fed4531728452a88f1d62ec78c992cd849aeb9e4
Racheal Ogungbile <rao22@njit.edu>
2024-06-30 14:17:56 -0400

    all tests pass pytest --pylint --cov with 93% coverage

--- tests/test_History.py
+++ tests/test_History.py
@@ -1,8 +1,12 @@
 '''My History Test'''
 
 from decimal import Decimal
+import pytest
+from faker import Faker
 from app.calculator.calculation import Calculation as calc
 from app.calculator.history import Calculator_History as log
+
+fake = Faker()
 
 def test_addcalc():
     '''Test that the calculation history addCalculation() method works ''' 
@@ -16,3 +20,26 @@ def test_getlastcalc():
     c = calc(Decimal('10'), Decimal('5'), 'add')
     log.addCalculation(c)
     assert log.getLastCalculation() == c
+
+@pytest.mark.parametrize("pos, cal", [
+    (int("0"), calc(Decimal('20'), Decimal('36'), 'add')),
+    (int("0"), calc(Decimal('4'), Decimal('5'), 'subtract'))
+])
+
+def test_removecalc(pos, cal):
+    '''Test that the calculation history removeCalculation() method works ''' 
+    log.emptyList()
+    log.addCalculation(cal)
+    temp = log.removeCalculation(pos)
+    assert f"{cal.x}, {cal.y}, {cal.comp}" == f"{temp.x}, {temp.y}, {temp.comp}"
+
+def test_emptylist():
+    '''Test that the calculation history emptyList() method works '''
+    calclist = [calc(Decimal(fake.random_number(digits=2)), Decimal(fake.random_number(digits=2)), 'add'),
+                calc(Decimal(fake.random_number(digits=2)), Decimal(fake.random_number(digits=2)), 'add'),
+                calc(Decimal(fake.random_number(digits=2)), Decimal(fake.random_number(digits=2)), 'subtract'),
+                calc(Decimal(fake.random_number(digits=2)), Decimal(fake.random_number(digits=2)), 'multiply'),]
+    for i in calclist:
+        log.addCalculation(i)
+    log.emptyList()
+    assert len(log.log) == 0
